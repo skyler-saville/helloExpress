1b3bb846bf60b44915da8117c1c300fb6bb6aa53
Skyler Saville <skylersaville@gmail.com>
2021-07-04 04:04:13 -0600

    passing time and geo data as json to timelog/ route

--- middleware/geolocate.js
+++ middleware/geolocate.js
@@ -1,14 +1,7 @@
 const fetch = require('node-fetch')
 
-const random_ip = [
-                    '24.76.44.172','174.69.182.202',
-                    '83.85.164.109','202.134.42.112',
-                    '123.205.18.180','252.247.218.97',
-                    '109.115.64.49','102.148.141.36', 
-                    '168.100.241.38','113.146.89.33'
-                ]
-let ip = random_ip[Math.floor(Math.random()*random_ip.length)] // default for when using localhost
-let info
+let ip = '113.146.89.33'
+
 const getIP = async function (req, res, next) {
  
     try{
@@ -17,19 +10,16 @@ const getIP = async function (req, res, next) {
             ip = req.connection.remoteAddress
         }
         console.log('fetching from ip-api.com...')
-        fetch(`http://ip-api.com/json/${ip}`)
-            .then(res=>res.json())
-            .then(json => info = json)
-            if (info ) {
-                req.geo = 'set'
-                req.geo.info = info
-    
-            }
+        const response = await fetch(`http://ip-api.com/json/${ip}`)
+        const json = await response.json()
+        if (json) {
+            console.log(`Response from ip-api: ${json}`)
+            req.geo = {data: json}
+        }
+
     } catch (err) {
         console.log(`${err.name}: ${err.message}`)
     }
-
     next()
-
 }
 module.exports = getIP
--- routes/timelog.js
+++ routes/timelog.js
@@ -5,15 +5,20 @@ const getIP = require('../middleware/geolocate')
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
     const now = new Date()
+    req.time = now
     console.log(`Time middleware: ${now.toUTCString()}`)
     next()
 })
 
 // define home route
 router.get('/', getIP, (req, res) => {
-    if (req.geo.info) {
+    if (req.geo) {
         console.log('the ip is set')
-        res.send(`req.geo.info is: ${req.geo.info}`)
+        const payload = {
+            time: req.time.toUTCString(),
+            ip_info: req.geo
+        }
+        res.send(payload)
     } else {
         res.send('Timelog home page')
     }
